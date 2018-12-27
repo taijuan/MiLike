ac4799a5de5bb6c2ef8e35e6c36228ac0a864208
zuiweng <673636090@qq.com>
2018-12-27 23:01:39 +0800

    url请求参数去重

--- app/src/main/java/com/milike/soft/utils/DataUtils.kt
+++ app/src/main/java/com/milike/soft/utils/DataUtils.kt
@@ -1,23 +1,37 @@
 package com.milike.soft.utils
 
+import android.net.Uri
 import com.blankj.utilcode.util.DeviceUtils
 import com.blankj.utilcode.util.SPUtils
 
 fun getWebUrlSuffix(url: String): String {
-//    val uri = Uri.parse(url)
-//    Uri.Builder()
-//        .scheme(uri.scheme)
-//        .authority(uri.authority)
-//        .path(uri.path)
-//        .query(uri.query)
-//        .q
-    val position = SPUtils.getInstance().getString("position")
-    return url + (if (url.contains("?")) "&" else "?") + "flag=milike" + position + getUIDAndBasicInfo()
-}
-
-private fun getUIDAndBasicInfo(): String {
-    val first = com.blankj.utilcode.util.SPUtils.getInstance().getInt("versionCode")
-    return "&mid=${DeviceUtils.getAndroidID()}&tm=${System.currentTimeMillis()}&appPlatform=android&initial=${first == -1}&uuid=${SPUtils.getInstance().getString(
-        "uuid"
-    )}"
+    val uri = Uri.parse(url)
+    val query = uri.queryParameterNames
+    val builder = Uri.Builder()
+        .scheme(uri.scheme)
+        .authority(uri.authority)
+        .path(uri.path)
+        .query(uri.query)
+    if (!query.contains("flag")) {
+        builder.appendQueryParameter("flag", "milike")
+    }
+    if (!query.contains("mid")) {
+        builder.appendQueryParameter("mid", DeviceUtils.getAndroidID())
+    }
+    if (!query.contains("tm")) {
+        builder.appendQueryParameter("tm", System.currentTimeMillis().toString())
+    }
+    if (!query.contains("appPlatform")) {
+        builder.appendQueryParameter("appPlatform", "android")
+    }
+    if (!query.contains("initial")) {
+        builder.appendQueryParameter("initial", (SPUtils.getInstance().getInt("versionCode") == -1).toString())
+    }
+    if (!query.contains("uuid")) {
+        builder.appendQueryParameter("uuid", SPUtils.getInstance().getString("uuid"))
+    }
+    if (!query.contains("token")) {
+        builder.appendQueryParameter("token", SPUtils.getInstance().getString("token"))
+    }
+    return builder.toString()
 }
